8e618e16bafd72e60947da5f3dac5cb6ee4607ad
VelproSpanking <2645600778@qq.com>
2022-06-27 20:46:40 +0800

    node后端

---
+++ school_second_hand/config/wx.js
@@ -0,0 +1,9 @@
+/**
+ * 微信公共平台：https://mp.weixin.qq.com/
+ * 开发者 => 开发者ID
+ */
+let wx = {
+	appid: "wx8532f70e46b2921d", //小程序ID
+	appSecret: "c164ef59bac4f7b0398be3d6b70f2f49", //小程序密钥
+}
+module.exports = wx;
